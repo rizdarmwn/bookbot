71d90f9621fdafdc461af8c478c8323d6ccfbe17
Rizdar <muhrizdar@gmail.com>
2025-12-05 10:09:52 +0700

    add accepting path arguments from cli

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_char_count, get_words_count, sorted_dict
 
 
@@ -8,22 +10,30 @@ def get_book_text(path):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_words_count(text)
-    chars = get_char_count(text)
-    sorted_chars = sorted_dict(chars)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    print(chars)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing boot found at {book_path}")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for c in sorted_chars:
-        if c["char"].isalpha():
-            print(f"{c['char']}: {c['num']}")
-    print("============= END ===============")
+    book_path = sys.argv[1]
+    try:
+        text = get_book_text(book_path)
+        num_words = get_words_count(text)
+        chars = get_char_count(text)
+        sorted_chars = sorted_dict(chars)
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing boot found at {book_path}")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for c in sorted_chars:
+            if c["char"].isalpha():
+                print(f"{c['char']}: {c['num']}")
+        print("============= END ===============")
+    except FileNotFoundError:
+        print("book not found, most likely path is incorrect")
+    except Exception as e:
+        print(e)
 
 
 main()
